704f0c3a953bfc35bd19b6985ba9af768d687dd4
Victor800augusto <victor.augusto800@gmail.com>
2022-08-16 13:54:59 -0300

    Adicionando requisições de livro

--- src/app/listar-livros/listar-livros.component.ts
+++ src/app/listar-livros/listar-livros.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Livro } from '../models/livro';
 import { LivroService } from '../services/livro-service/livro-service.service';
 
 @Component({
@@ -7,12 +8,31 @@ import { LivroService } from '../services/livro-service/livro-service.service';
   styleUrls: ['./listar-livros.component.css'],
 })
 export class ListarLivrosComponent implements OnInit {
+  livros: Livro[] = [];
+
   constructor(private livroService: LivroService) {}
 
   ngOnInit(): void {
     this.livroService.getAll().subscribe({
       next: (success) => {
+        this.livros = success;
+      },
+      error: (error) => {
+        console.log('deu erro');
+      },
+    });
+  }
+
+  RemoverLivro(livro: Livro) {
+    console.log(livro.id);
+    this.livroService.removeLivro(livro.id).subscribe({
+      next: (success) => {
         console.log(success);
+        console.log('livro ' + livro.titulo + 'excluido com sucesso');
+
+      },
+      error: (error) => {
+        console.log(error);
       },
     });
   }
--- src/app/services/livro-service/livro-service.service.ts
+++ src/app/services/livro-service/livro-service.service.ts
@@ -1,9 +1,10 @@
+import { Livro } from './../../models/livro';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { Observable } from 'rxjs';
 
-const URL_API = environment.URL_API + '/livros';
+const URL_API = environment.URL_API + 'livros';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,11 @@ export class LivroService {
 
   constructor(private http: HttpClient) {}
 
-  getAll(): Observable<any>{
-    return this.http.get(URL_API);
+  getAll(): Observable<Livro[]>{
+    return this.http.get<Livro[]>(URL_API);
+  }
+
+  removeLivro(id:number): Observable<void>{
+    return this.http.delete<void>(URL_API + "/" + id)
   }
 }
