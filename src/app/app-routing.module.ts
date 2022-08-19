da8adf5d7dd120992c24b3eda55ddbfb82e5dea5
Victor800augusto <victor.augusto800@gmail.com>
2022-08-19 08:18:19 -0300

    Corrigindo bootstrap e adicionando rota para formulario de livro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,30 @@
+import { FormularioLivroComponent } from './formulario-livro/formulario-livro.component';
 import { ListarLivrosComponent } from './listar-livros/listar-livros.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{
-  path: 'livros', component: ListarLivrosComponent
-}];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'livros',
+  },
+  {
+    path: 'livros',
+    component: ListarLivrosComponent,
+  },
+  {
+    path: 'cria-livro',
+    component: FormularioLivroComponent,
+  },
+  {
+    path: 'altera-livro',
+    component: FormularioLivroComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule],
 })
-
-export class AppRoutingModule { }
+export class AppRoutingModule {}
